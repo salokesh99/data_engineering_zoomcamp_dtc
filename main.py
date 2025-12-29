be3302c4856360e7c3d5182e91b2a0a613a42226
Lsn-User <lsn-user@LSN-ITs-MacBook-Air-8.local>
2025-12-30 01:42:33 +0530

    added uv dependencies

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from data-talks!")
+
+
+if __name__ == "__main__":
+    main()
--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,19 @@ import sys
 
 import pandas as pd
 
-df = pd.DataFrame
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+
+
+month = sys.argv[2]
+
+
+df['month'] = month
+
+print(df.head())
+
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
 
 # Our pipeline here
 
