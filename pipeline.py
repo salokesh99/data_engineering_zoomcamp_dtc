2c6b389f6dc36c8cf05479d46506815b542f4518
Lsn-User <lsn-user@LSN-ITs-MacBook-Air-8.local>
2025-12-30 01:01:03 +0530

    initial commit

---
+++ pipeline.py
@@ -0,0 +1,13 @@
+import sys
+
+import pandas as pd
+
+df = pd.DataFrame
+
+# Our pipeline here
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+print('Completed the task successfully!!!! for the day {}'.format(day))
